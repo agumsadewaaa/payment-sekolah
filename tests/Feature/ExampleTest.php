458d26239f1962796df93fd4c9f513225adf10a2
agumsadewaaa <agumsadewaa@gmail.com>
2025-11-30 12:29:56 +0700

    test: update ExampleTest to expect redirect for unauthenticated home

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        // Home requires auth so a guest will be redirected to login
+        $response->assertStatus(302);
     }
 }
