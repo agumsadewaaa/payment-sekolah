bbedd9a957d3b4411c76b2424a67414679fa7861
agumsadewaaa <agumsadewaa@gmail.com>
2025-12-03 19:59:49 +0700

    feat: auto-generate class code format (10-TKJ) with jurusan dropdown

--- resources/views/kelas/fields.blade.php
+++ resources/views/kelas/fields.blade.php
@@ -1,12 +1,16 @@
-<!-- Kode Field (Hidden, auto-generated) -->
-{!! Form::hidden('kode', null, ['id' => 'kode']) !!}
+<!-- Kode Field (Read-only preview) -->
+<div class="form-group col-sm-12">
+    {!! Form::label('kode_preview', 'Kode Kelas (Auto):') !!}
+    <input type="text" id="kode_preview" class="form-control bg-light" readonly placeholder="Akan otomatis terisi (contoh: 10-TKJ)">
+    {!! Form::hidden('kode', null, ['id' => 'kode']) !!}
+</div>
 
 <!-- Kelas Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('kelas', 'Kelas:') !!}
+    {!! Form::label('kelas', 'Tingkat Kelas:') !!}
     {!! Form::select('kelas', [10 => '10', 11 => '11', 12 => '12'], null, [
         'class' => 'form-control',
-        'placeholder' => 'Pilih kelas',
+        'placeholder' => 'Pilih tingkat kelas',
         'required',
         'id' => 'kelas'
     ]) !!}
@@ -15,25 +19,49 @@
 <!-- Jurusan Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('jurusan', 'Jurusan:') !!}
-    {!! Form::text('jurusan', null, ['class' => 'form-control', 'required', 'id' => 'jurusan']) !!}
+    {!! Form::select('jurusan', [
+        'Akuntansi dan Keuangan Lembaga' => 'Akuntansi dan Keuangan Lembaga',
+        'Manajemen Perkantoran dan Layanan Bisnis' => 'Manajemen Perkantoran dan Layanan Bisnis',
+        'Teknologi Farmasi' => 'Teknologi Farmasi',
+        'Teknik Jaringan Komputer & Telekomunikasi' => 'Teknik Jaringan Komputer & Telekomunikasi',
+        'Teknik Otomotif' => 'Teknik Otomotif'
+    ], null, [
+        'class' => 'form-control',
+        'placeholder' => 'Pilih jurusan',
+        'required',
+        'id' => 'jurusan'
+    ]) !!}
 </div>
 
 @push('scripts')
 <script>
     $(document).ready(function () {
-        // Auto-generate kode dari kelas + jurusan
+        // Mapping jurusan ke kode singkat
+        const jurusanMap = {
+            'Akuntansi dan Keuangan Lembaga': 'AKL',
+            'Manajemen Perkantoran dan Layanan Bisnis': 'MPLB',
+            'Teknologi Farmasi': 'TF',
+            'Teknik Jaringan Komputer & Telekomunikasi': 'TJKT',
+            'Teknik Otomotif': 'TO'
+        };
+
+        // Auto-generate kode dari kelas + kode singkat jurusan
         function updateKode() {
             var kelasVal = $('#kelas').val();
             var jurusanVal = $('#jurusan').val();
 
             if (kelasVal && jurusanVal) {
-                $('#kode').val(kelasVal + '-' + jurusanVal);
+                var kodeJurusan = jurusanMap[jurusanVal] || '';
+                var kodeKelas = kelasVal + '-' + kodeJurusan;
+                $('#kode').val(kodeKelas);
+                $('#kode_preview').val(kodeKelas);
             } else {
                 $('#kode').val('');
+                $('#kode_preview').val('');
             }
         }
 
-        $('#kelas, #jurusan').on('change keyup', function() {
+        $('#kelas, #jurusan').on('change', function() {
             updateKode();
         });
 
